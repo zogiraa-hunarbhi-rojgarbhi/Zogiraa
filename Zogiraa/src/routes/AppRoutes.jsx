dcda7651e3013fa24828d3c055cc62ca36871d89
Rupak Chatterjee <147531801+LIGHT-25@users.noreply.github.com>
2026-02-17 22:00:55 +0530

    Fixed AppRoutes default export issue

--- Zogiraa/src/routes/AppRoutes.jsx
+++ Zogiraa/src/routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import MainLayout from "../layouts/MainLayout";
 import BlankLayout from "../components/Footer/Knowledge_Hub/Layouts/BlankLayout";
 
 import HomePage from "../pages/Home/HomePage";
-
 import Contact from "../pages/ContactUs/Contact";
 import HowItWorks from "../pages/HowItWorks/HowItWorks";
 import TrainingPage from "../pages/Training/TrainingPage";
@@ -15,22 +14,28 @@ import EmployerTips from "../components/Footer/Knowledge_Hub/EmployerTips";
 import JobReadiness from "../components/Footer/Knowledge_Hub/JobReadiness";
 import GovernmentSchemes from "../components/Footer/Knowledge_Hub/GovernmentSchemes";
 
-<Routes>
-  {/* Main Layout (with header + footer) */}
-  <Route path="/" element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path="contact-us" element={<Contact />} />
-    <Route path="how-it-works" element={<HowItWorks />} />
-    <Route path="training" element={<TrainingPage />} />
-    <Route path="user-policy" element={<UserPolicy />} />
-    <Route path="partner-with-us" element={<PartnershipPage />} />
-  </Route>
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* Main Layout */}
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="contact-us" element={<Contact />} />
+        <Route path="how-it-works" element={<HowItWorks />} />
+        <Route path="training" element={<TrainingPage />} />
+        <Route path="user-policy" element={<UserPolicy />} />
+        <Route path="partner-with-us" element={<PartnershipPage />} />
+      </Route>
 
-  {/* Knowledge Pages (no header/footer) */}
-  <Route element={<BlankLayout />}>
-    <Route path="/knowledge/blog" element={<Blog />} />
-    <Route path="/knowledge/employers" element={<EmployerTips />} />
-    <Route path="/knowledge/job-readiness" element={<JobReadiness />} />
-    <Route path="/knowledge/government-schemes" element={<GovernmentSchemes />} />
-  </Route>
-</Routes>
+      {/* Knowledge Pages */}
+      <Route element={<BlankLayout />}>
+        <Route path="/knowledge/blog" element={<Blog />} />
+        <Route path="/knowledge/employers" element={<EmployerTips />} />
+        <Route path="/knowledge/job-readiness" element={<JobReadiness />} />
+        <Route path="/knowledge/government-schemes" element={<GovernmentSchemes />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
